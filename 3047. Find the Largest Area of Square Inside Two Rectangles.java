659a29d51001d7b2cd7eec8a4b9ad5b638edc87c
PiyushKumar495 <92709285+PiyushKumar495@users.noreply.github.com>
2026-01-17 10:50:28 +0530

    Create 3047. Find the Largest Area of Square Inside Two Rectangles.java

---
+++ 3047. Find the Largest Area of Square Inside Two Rectangles.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long largestSquareArea(int[][] bottomLeft, int[][] topRight) {
+        long area = 0;
+        int n = bottomLeft.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int left   = Math.max(bottomLeft[i][0], bottomLeft[j][0]);
+                int bottom = Math.max(bottomLeft[i][1], bottomLeft[j][1]);
+                int right  = Math.min(topRight[i][0], topRight[j][0]);
+                int top    = Math.min(topRight[i][1], topRight[j][1]);
+                if (left < right && bottom < top) {
+                    int side = Math.min(right - left, top - bottom);
+                    area = Math.max(area, (long) side * side);
+                }
+            }
+        }
+        return area;
+    }
+}
