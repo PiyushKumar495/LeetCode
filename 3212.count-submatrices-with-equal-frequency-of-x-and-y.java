2b1eb28f0c067ec1d9cf5f5ee433f1a15a0592b6
PiyushKumar495 <92709285+PiyushKumar495@users.noreply.github.com>
2026-03-19 09:26:59 +0530

    Create 3212.count-submatrices-with-equal-frequency-of-x-and-y.java

---
+++ 3212.count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numberOfSubmatrices(char[][] grid) {
+       int[] countX = new int[grid[0].length];
+       int[] countY = new int[grid[0].length];
+       int num=0;
+       for(int i=0;i<grid.length;i++){
+
+        int x=0;
+        int y=0;
+        for(int j=0;j<grid[0].length; j++){
+
+             if(grid[i][j] == 'X')
+                x++;
+             if(grid[i][j]== 'Y')
+                y++;
+            countX[j] +=x;
+            countY[j] +=y;
+            if(countX[j]>=1 && (countX[j]==countY[j])){
+                num++;
+            }
+        }
+        }
+
+    return num;
+
+       
+    }
+}
